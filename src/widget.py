015ab845e9b582459c7ea8ececf94c20cdb5e30d
LeeVed <leeveden2008@gmail.com>
2025-07-25 11:31:33 +0300

    fixes_widget

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from src.masks import get_mask_account, get_mask_card_number
+from src.masks import get_mask_account
+from src.masks import get_mask_card_number
 
 
 def mask_account_card(card_account_number: str) -> str:
@@ -6,15 +7,15 @@ def mask_account_card(card_account_number: str) -> str:
     возвращает строку с замаскированным номером"""
     index_digit = card_account_number.rfind(" ")
     if "Счет" in card_account_number[:index_digit]:
-        mask_account = get_mask_account(card_account_number[index_digit + 1:])
-        return card_account_number[:index_digit + 1] + mask_account
+        mask_account = get_mask_account(card_account_number[index_digit + 1 :])
+        return card_account_number[: index_digit + 1] + mask_account
     else:
-        mask_card_number = get_mask_card_number(card_account_number[index_digit + 1:])
-        return card_account_number[:index_digit + 1] + mask_card_number
+        mask_card_number = get_mask_card_number(card_account_number[index_digit + 1 :])
+        return card_account_number[: index_digit + 1] + mask_card_number
 
 
 def get_date(data_str: str) -> str:
-    """ Функция принимает строку и возвращает строку с датой в формате 'ДД.ММ.ГГГГ' """
+    """Функция принимает строку и возвращает строку с датой в формате 'ДД.ММ.ГГГГ'"""
     date = data_str[:10]
     date_split = date.split("-")
     return f"{date_split[2]}.{date_split[1]}.{date_split[0]}"
