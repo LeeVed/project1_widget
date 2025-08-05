cb35a984c8e7fc88a9544fd2e8f173cefe9fb78e
LeeVed <leeveden2008@gmail.com>
2025-08-05 22:58:43 +0300

    test_func_for_masks_created

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,28 @@
+import pytest
 from src.masks import get_mask_card_number
 from src.masks import get_mask_account
 
 
-def test_get_mask_card_number():
-    pass
+@pytest.mark.parametrize("card_number, expected", [("1234567890123456", "1234 56** **** 3456"),
+                                                   ("9876543210987654", "9876 54** **** 7654"),
+                                                   ("4567890123456789", "4567 89** **** 6789"),
+                                                   ("1111111111111111", "1111 11** **** 1111"),
+                                                   ("0000000000000000", "0000 00** **** 0000"),
+                                                   ("123456789012345", "Некорректный номер карты"),
+                                                   ("12c345678901234a", "Некорректный номер карты")])
+def test_get_mask_card_number(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
 
 
-def test_get_mask_account():
-    pass
+@pytest.mark.parametrize("account_number, expected", [("1234567890123456", "**3456"),
+                                                   ("9876543210987654", "**7654"),
+                                                   ("4567890123456789", "**6789"),
+                                                   ("1111111111111111", "**1111"),
+                                                   ("0000000000000000", "**0000"),
+                                                   ("123456789012345", "Некорректный номер счета"),
+                                                   ("12c345678901234a", "Некорректный номер счета")])
+def test_get_mask_account(account_number, expected):
+    assert get_mask_account(account_number) == expected
+
+
+
