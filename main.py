f92927ccd9405d6491eec394358f1db114865df3
LeeVed <leeveden2008@gmail.com>
2025-08-09 16:44:45 +0300

    test processing add

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ list_state = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29
               {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
               {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 if __name__ == '__main__':
-    print(filter_by_state(list_state, key_state='CANCELED'))
-    print(filter_by_state(list_state))
+    #print(filter_by_state(list_state, key_state='CANCELED'))
+    #print(filter_by_state(list_state))
+    print(sort_by_date(list_state, sorting=False))
