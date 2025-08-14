3b7986e6c81b3058e5b9c404b3d593aeadf53076
LeeVed <leeveden2008@gmail.com>
2025-08-14 16:19:00 +0300

    file generators.py

---
+++ src/generators.py
@@ -0,0 +1,27 @@
+from typing import Generator, Iterator
+
+
+def filter_by_currency(transactions: list, currency: str) -> Iterator:
+    """Функция принимает на вход список словарей, представляющих транзакции и
+       возвращает итератор, который поочередно выдает транзакции,
+       где валюта операции соответствует заданной (например, USD)."""
+    counter = 0
+    for transaction in transactions:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
+            counter += 1
+            yield transaction
+
+
+def transaction_descriptions(transactions: list) -> Iterator:
+    """Функция принимает список словарей с транзакциями и возвращает описание каждой операции."""
+    for transaction in transactions:
+        yield transaction["description"]
+
+
+def card_number_generator(start: int, stop: int) -> Generator:
+    """Функция генерирует номера банковских карт в формате XXXX XXXX XXXX XXXX, где X— цифра номера карты
+     и в диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999."""
+    for card in range(start, stop + 1):
+        card_str = str(card).zfill(16)
+        formatted_card = f"{card_str[:4]} {card_str[4:8]} {card_str[8:12]} {card_str[12:]}"
+        yield formatted_card
--- tests/conftest.py
+++ tests/conftest.py
@@ -45,3 +45,43 @@ def sort_false():
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     ]
+
+
+@pytest.fixture
+def transaction_usd():
+    return [
+        {"id": 1, "operationAmount": {"amount": "150", "currency": {"code": "USD"}}},
+        {"id": 3, "operationAmount": {"amount": "300", "currency": {"code": "USD"}}}
+    ]
+
+
+@pytest.fixture
+def transactions_other_currency():
+    return [
+        {"id": 2, "operationAmount": {"amount": "200", "currency": {"code": "EUR"}}}
+    ]
+
+
+@pytest.fixture
+def transactions_no_usd():
+    return [
+        {"id": 4, "operationAmount": {"amount": "500", "currency": {"code": "EUR"}}},
+        {"id": 5, "operationAmount": {"amount": "1000", "currency": {"code": "GBP"}}}
+    ]
+
+
+@pytest.fixture
+def transactions_empty_list():
+    return []
+
+
+@pytest.fixture
+def transactions_data():
+    return [
+        {"id": 1, "description": "Перевод организации"},
+        {"id": 2, "description": "Перевод с карты на счет"},
+        {"id": 3, "description": "Перевод со счета на счет"},
+        {"id": 4, "description": "Перевод со счета на карту"},
+        {"id": 5, "description": "Перевод с карты на карту"}
+    ]
+
